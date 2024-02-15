2a9ca25b9c85b12e409afdfe034476b38c0fa284
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-02-16 00:14:43 +0100

    add new task

---
+++ essential/oop/conceptions/inheritance/example.php
@@ -0,0 +1,57 @@
+<?php
+
+class Car
+{
+  public $brand;
+  public $color;
+  public $maxSpeed;
+  public $currentSpeed = 0;
+
+  public function __construct($brand, $color = "black", $maxSpeed = 500)
+  {
+    $this->brand = $brand;
+    $this->color = $color;
+    $this->maxSpeed = $maxSpeed;
+  }
+
+  public function move($speed)
+  {
+    $this->currentSpeed = $speed;
+  }
+
+  public function stop()
+  {
+    $this->currentSpeed = 0;
+  }
+}
+
+class Mitsubishi extends Car
+{
+  public $sign = 'M';
+  // public function __construct($brand, $color, $maxSpeed)
+  // {
+  //   parent::__construct($brand, $color, $maxSpeed);
+  // }
+
+  // public function __construct()
+  // {
+  //   parent::__construct('Mitsubishi', 'Black', 460);
+  // }
+
+  public function __construct($color = 'black', $maxSpeed = 500)
+  {
+    parent::__construct('Mitsubishi', $color, $maxSpeed);
+  }
+
+}
+
+$audi = new Car('AUDI', 'white', 550);
+$audi->move(60);
+
+$mitsubishi = new Mitsubishi();
+$mitsubishi->move(80);
+
+echo "<pre>";
+var_dump($audi);
+var_dump($mitsubishi);
+echo "</pre>";
