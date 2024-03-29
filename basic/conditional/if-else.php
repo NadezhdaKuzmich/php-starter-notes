e2bdf256d38455ece6f7603aeb9907fc2f807861
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-03-29 21:59:58 +0100

    add new theme

--- basic/conditional/if-else.php
+++ basic/conditional/if-else.php
@@ -1,16 +1,22 @@
 <?php
 
-// Інструкція if-else:
-// Умовна конструкція if-else дозволяє направити роботу програми в залежності 
-// від умови по одному з можливих шляхів.
-// if (логічне_вираз) {
-//   інструкція_1;
-// } else {
-//   інструкція_2;
-// }
-
 /*
-Керуючі конструкції
+Керуючі конструкції:
+
+Інструкція if-else
+- Умовна конструкція if-else дозволяє направити роботу програми 
+в залежності від умови по одному з можливих шляхів.
+
+if (логічне_вираз) {
+  інструкція_1;
+} else {
+  інструкція_2;
+}
+
+Note: also the left curly brace {. This symbol denotes a block of 
+multiple lines of code. Without it, the conditional would only refer 
+to the statement immediately following it. It is a good practice to 
+always use the braces.
 */
 
 // Інструкція if
--- basic/conditional/if-elseif-else.php
+++ basic/conditional/if-elseif-else.php
@@ -8,7 +8,6 @@
 
 // Інструкція if-elseif-else
 $hour = 12;
-
 if ($hour < 12) {
   echo "Ще ранок!";
 } elseif ($hour == 12) {
@@ -16,11 +15,9 @@ if ($hour < 12) {
 } else {
   echo "Уже вечір!";
 }
-echo "<br>";
-
+echo "<hr>";
 
 $money = 32;
-
 if ($money < 20) {
   echo "У вас недостатньо грошей";
 } elseif ($money < 25) {
@@ -30,3 +27,50 @@ if ($money < 20) {
 } else {
   echo "Перегляньте інші товари";
 }
+echo "<hr>";
+
+$score = 50;
+if ($score > 100) {
+  echo "Error: the score is greater than 100!\n";
+} else if ($score < 0) {
+  echo "Error: the score is less than 0!\n";
+} else if ($score >= 50) {
+  echo "Pass!\n";
+} else {
+  echo "Fail!\n";
+}
+echo "<hr>";
+
+$a = 5;
+$b = 10;
+// Correct Method:
+if ($a > $b):
+  echo $a . " is greater than " . $b;
+elseif ($a == $b): // Note the combination of the words.
+  echo $a . " equals " . $b;
+else:
+  echo $a . " is neither greater than or equal to " . $b;
+endif;
+
+// Incorrect Method:
+// if ($a > $b):
+//     echo $a." is greater than ".$b;
+// else if ($a == $b): // Will not compile.
+//     echo "The above line causes a parse error.";
+// endif;
+
+// The parser doesn't handle mixing alternative if syntaxes as reasonably as possible.
+// The following codes are illegal (as it should be):
+// $a = true;
+// if($a) {
+//   echo $a;
+// }
+// else:
+//   echo $c;
+// endif;
+
+// if($a):
+//   echo $a;
+// else {
+//   echo $c;
+// }
