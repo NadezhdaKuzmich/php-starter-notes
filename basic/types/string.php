31663e6e54b9782c98c3f1ac053aa481ac515ab9
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-05 21:30:49 +0200

    edit file, add more tasks

--- basic/types/string.php
+++ basic/types/string.php
@@ -19,6 +19,8 @@ echo "<pre>";
 $name = "Nadiia";
 $secondName = "Kuzmich";
 // You can use variables in your string (in ""):
+// If we use single quotes, the compiler will output $name as the 
+// raw text (without interpreting it).
 echo "1. Привіт $name $secondName";
 echo '<br>';
 // You can use variables \n and so on:
@@ -28,12 +30,34 @@ echo "3. Якось Арнольд сказав: \"I'll be back\"\n";
 // экранирование $ (знака доллара)
 echo "4. Ім'я зберігається у змінній \$name";
 echo "</pre>";
-echo '<br>';
+echo '<hr>';
 
 $dog_name = "Tadpole";
 $favorite_food = "treat";
 $color = "brown";
-echo "I have a {$color}ish dog named $dog_name and her favorite food is {$favorite_food}s.";
+// In PHP you can also use the complex syntax, or the curly syntax which 
+// requires that you wrap your variable within curly braces {}. This can 
+// be useful when embedding variables within the text and helps prevent 
+// possible ambiguity between text and variables. 
+echo "I have a {$color}ish dog named $dog_name and her favorite food is {$favorite_food}s. <hr>";
+
+// The {} syntax only interpolates variables starting with a $ into a string:
+echo "The {} syntax doesn't interpolate variables without a $: <br>";
+
+// trying to interpolate a PHP expression
+echo "1 + 2 = {1 + 2}<br>";
+
+// using a constant
+define("HELLO_WORLD", "Hello World!!");
+echo "My constant is {HELLO_WORLD}.<br>";
+
+// Example using a function
+function say_hello()
+{
+  return "Hello!";
+}
+;
+echo "I say: {say_hello()}<hr>";
 
 /*
 Heredoc (like string in "")
@@ -57,9 +81,21 @@ echo '<br><hr>';
 
 // strlen - повертає довжину рядка.
 echo strlen('Hello');
+echo '<br>';
+
+$my_str = 'Welcome to PHP!';
+echo strlen($my_str);
+echo '<hr>';
+
+// str_word_count() function counts the number of words in a string.
+$my_str = 'The quick brown fox jumps over the lazy dog.';
+echo str_word_count($my_str);
 echo '<hr>';
 
 // strpos - повертає позицію першого входження підрядка.
+echo "The occurence of 'hay' is at position: " . strpos("haystack", "hay") . "<br>";
+echo "The occurence of 'stack' is at position: " . strpos("haystack", "stack") . "<br>";
+
 $mystring = 'abc';
 $findme = 'a';
 $pos = strpos($mystring, $findme);
@@ -69,9 +105,18 @@ $pos = strpos($mystring, $findme);
 if ($pos === false) {
   echo "Рядок '$findme' не знайдено в рядку '$mystring'";
 } else {
-  echo "Рядок '$findme' знайдено в рядку '$mystring'";
-  echo " у позиції $pos";
+  echo "Рядок '$findme' знайдено в рядку '$mystring' у позиції $pos";
 }
+echo '<hr>';
+
+function stringCheck($str1, $str2)
+{
+  return strpos($str1, $str2) !== false ? 1 : 0;
+}
+
+echo stringCheck('The quick brown fox jumps over the lazy dog.', 'fox');
+echo '<br>';
+echo stringCheck('The quick brown fox jumps over the lazy dog.', 'cat');
 echo '<hr>';
 
 // stripos - повертає позицію першого входження підрядка 
@@ -94,12 +139,29 @@ if ($pos2 !== false) {
 }
 echo '<hr>';
 
+/*
+Single characters can be extracted using array (square brace) 
+syntax as well as curly brace syntax. These two syntaxes will 
+only return a single character from the string. 
+BUT: 
+'curly brace array access syntax' is deprecated since PHP 8.0
+If we need more than one character, we have to use the in-built 
+substr function.
+*/
+
+$foo = 'Hello world';
+echo $foo[6];
+// echo $foo{6}; // Fatal error: ...no longer supported
+echo '<hr>';
+
 // substr - повертає підрядок.
-$rest = substr("abcdef", -1);
-echo $rest;
+echo substr($foo, 6, 1);
 echo '<br>';
-$rest = substr("abcdef", -2);
-echo $rest;
+echo substr($foo, 6, 2);
+echo '<br>';
+echo substr("abcdef", -1);
+echo '<br>';
+echo substr("abcdef", -2);
 echo '<br>';
 $rest = substr("abcdef", -3, 1);
 echo $rest;
@@ -143,20 +205,41 @@ echo substr_count($text, 'is', 3, 3);
 // echo substr_count($text, 'is', 5, 10);
 echo '<hr>';
 
-// str_replace - замінює всі входження рядка пошуку на 
-// рядок заміни.
+/*
+Strings can also be changed one character at a time using the 
+same square brace. Replacing more than one character requires 
+a function, substr_replace.
+*/
+$foo = 'hello world';
+$foo[6] = 'W';
+echo $foo;
+echo '<hr>';
+
+// str_replace - замінює всі входження рядка пошуку на рядок заміни.
+$my_str = 'If the facts do not fit the theory, change the facts.';
+echo str_replace("facts", "truth", $my_str);
+echo '<br>';
+
 $vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
 echo $onlyconsonants = str_replace($vowels, "", "Hello World of PHP");
-
 echo '<br>';
+
 $phrase = "You should eat fruits, vegetables, and fiber every day.";
 $healthy = array("fruits", "vegetables", "fiber");
 $yummy = array("pizza", "beer", "ice cream");
+
 echo $newphrase = str_replace($healthy, $yummy, $phrase);
 echo '<br>';
 
+// You can optionally pass the fourth argument to know how many 
+// times the string replacements were performed, like so:
 $str = str_replace("ll", "", "good golly miss molly!", $count);
 echo $count;
+echo '<br>';
+
+$my_str = 'If the facts do not fit the theory, change the facts.';
+str_replace("facts", "truth", $my_str, $count);
+echo "The text was replaced $count times.";
 echo '<hr>';
 
 // str_split - перетворює рядок у масив.
@@ -170,9 +253,25 @@ echo '<hr>';
 
 // strrev - перевертає рядок задом наперед.
 echo strrev("Hello world!");
+echo '<br>';
+
+$my_str = 'You can do anything, but not everything.';
+echo strrev($my_str);
 echo '<hr>';
 
 // substr_replace - замінює частину рядка.
+// Note: The substr_replace function does not change 
+// the actual string. It just returns the new string 
+// that would’ve been made after doing the replacement 
+// under discussion.
+$bar = substr_replace($foo, '!', 11, 1);
+echo $bar;
+echo '<br>';
+
+$bar = substr_replace($foo, 'Whi', 6, 2);
+echo $bar;
+echo '<br>';
+
 $var = 'ABCDEFGH:/MNRPQR/';
 echo "Оригінал: $var<hr />\n";
 
