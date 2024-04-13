5fee245cbb90503d4138dbb39f2b7a69102ec067
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-13 22:25:22 +0200

    add new task

--- essential/oop/conceptions/inheritance/example2.php
+++ essential/oop/conceptions/inheritance/example2.php
@@ -1,1 +1,43 @@
 <?php
+class BaseShape
+{
+  public $sides = 0;
+  public $name = " ";
+
+  public function __construct($name, $sides)
+  {
+    $this->sides = $sides;
+    $this->name = $name;
+  }
+
+  public function description()
+  {
+    return "A $this->name with $this->sides sides.";
+  }
+}
+
+class Square extends BaseShape
+{
+  public $sideLength = 0;
+
+  public function __construct($sideLength)
+  {
+    parent::__construct("square", 4); //calling parent class constructor
+    $this->sideLength = $sideLength;
+  }
+
+  public function perimeter()
+  {
+    return $this->sides * $this->sideLength;
+  }
+
+  public function area()
+  {
+    return $this->sideLength * $this->sideLength;
+  }
+}
+
+$mySquare = new Square(10);
+echo $mySquare->description() . "<br>";
+echo "Perimeter of the square is: " . $mySquare->perimeter() . "<br>";
+echo "Area of the square is: " . $mySquare->area();
