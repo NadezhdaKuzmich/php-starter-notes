594474a8c1c35cf5fdaf7b517e4a2b4ec6b3fe2f
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-26 22:59:42 +0200

    edit file

--- basic/functions/build-in/data&time-func.php
+++ basic/functions/build-in/data&time-func.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Date
+- Date -
 'd' = Day
 'j' = Day without zeros
 'D' = Day of week (3 letters)
@@ -12,19 +12,7 @@ Date
 'F' = Full month
 'y' = Two-digit year
 'Y' = Full year
-
-Times
-'g' = Hours in 12-hour format without zeros
-'h' = Hours in 12 hour format with zeros
-'G' = hours in 24-hour format without zeros
-'H' = Hours in 24-hour format with zeros
-'a' = am/pm in lowercase
-'A' = am/pm in uppercase
-'i' = minutes without leading zeros
-'s' = seconds without leading zeros
 */
-
-// DATE
 echo date('d') . '<br>';
 echo date('D') . '<br>';
 echo date('l') . '<br>';
@@ -38,3 +26,17 @@ echo date('Y') . '<hr>';
 echo date('l, d F') . '<hr>';
 echo date('d/m/Y') . '<hr>';
 
+/*
+- Time -
+'g' = Hours in 12-hour format without zeros
+'h' = Hours in 12 hour format with zeros
+'G' = hours in 24-hour format without zeros
+'H' = Hours in 24-hour format with zeros
+'a' = am/pm in lowercase
+'A' = am/pm in uppercase
+'i' = minutes without leading zeros
+'s' = seconds without leading zeros
+*/
+echo date('H:i:s') . '<br>';
+echo date('h:i:s a') . '<br>';
+echo date('h:i:s A') . '<hr>';
