dacab83141d419a4bfd472ac9d7f46027712a1fb
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-27 22:00:00 +0200

    add new tasks

--- basic/tasks/task4.php
+++ basic/tasks/task4.php
@@ -27,9 +27,29 @@ $targetDays = mktime(0, 0, 0, 07, 05, 2024);
 $today = time();
 
 $differenceDays = $targetDays - $today;
-$days = (int)($differenceDays / 86400);
+$days = (int) ($differenceDays / 86400);
 
 echo "<b>Task 2:</b><br>";
 echo "My birthday is in $days days!";
 echo '<hr>';
+
+// task 3
+$randNumber = rand(0, 100) / 10;
+echo "<b>Task 3:</b><br>";
+echo "Random float number: $randNumber";
+echo '<hr>';
+
+// task 4
+$str = "My name is Nadiia. I'm 27 years old and I live in Poland. 
+I'm getting pretty old. I like PHP, because it's a programming language.";
+
+function checkLowerCase($str)
+{
+  return strtolower($str);
+}
+
+echo "<b>Task 3:</b><br>";
+echo checkLowerCase($str);
+echo '<hr>';
+
 echo '</pre>';
