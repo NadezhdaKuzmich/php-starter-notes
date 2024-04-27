47b939aaa03c06311ca93652a1a701fc363c620f
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-27 21:49:33 +0200

    edit file

--- basic/tasks/task4.php
+++ basic/tasks/task4.php
@@ -19,13 +19,17 @@ print_r($numbers);
 echo '<hr>';
 
 // task 2
+// mktime - повертає тимчасову мітку Unix відповідно до переданих аргументів 
+// або false, якщо тимчасову мітку не можна представити у вигляді цілого числа.
 $targetDays = mktime(0, 0, 0, 07, 05, 2024);
+
+// time - повертає поточний час у форматі Unix timestamp.
 $today = time();
 
 $differenceDays = $targetDays - $today;
 $days = (int)($differenceDays / 86400);
 
 echo "<b>Task 2:</b><br>";
-echo "Days till next birthday: $days days!";
+echo "My birthday is in $days days!";
 echo '<hr>';
 echo '</pre>';
