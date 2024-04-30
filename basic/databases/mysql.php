f8dd403290eb6c25ddaf3d0043062e8ffd0a2320
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-30 22:10:24 +0200

    new theme

---
+++ basic/databases/mysql.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+- Create table:
+CREATE TABLE users(
+  id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL
+);
+
+- Incert into table:
+INSERT INTO users (username, password) VALUES ('Mary', '121212');
+- more than one value:
+INSERT INTO users (username, password) VALUES ('Michael', '343434'), ('Kate', '454545');
+- error:
+INSERT INTO users (username, password) VALUES (͟A͟n͟y͟b͟o͟d͟y͟,͟ ͟3͟4͟3͟4͟3͟4͟)͟;
+
+*/
