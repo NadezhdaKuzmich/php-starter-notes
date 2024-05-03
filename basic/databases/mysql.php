0325f98e1f04b85fe52e4589cfbb862c49803c77
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-05-03 21:31:01 +0200

    edit file

--- basic/databases/mysql.php
+++ basic/databases/mysql.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+
+require_once 'project/includes/database.php';
 
 /*
 - Create table:
@@ -47,3 +49,24 @@ SELECT * FROM users ORDER BY id ASC;
 - or
 SELECT * FROM users ORDER BY username ASC;
 */
+
+$sql = 'SELECT * FROM users';
+// $sql = 'SELECT * FROM users WHERE id = 1';
+// $sql = 'SELECT * FROM users WHERE id = 3';  // No results found.
+
+
+// mysqli_query виконує запит query до бази даних.
+$result = mysqli_query($conn, $sql);
+
+// mysqli_num_rows повертає число рядів у результуючій вибірці.
+$rowCount = mysqli_num_rows($result);
+
+if ($rowCount) {
+  // mysql_fetch_assoc - обробляє ряд результату запиту 
+  // і повертає асоціативний масив.
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['id'] . '. ' . $row['username'] . '<br>';
+  }
+} else {
+  echo 'No results found.';
+}
--- basic/databases/project/register.php
+++ basic/databases/project/register.php
@@ -5,11 +5,11 @@ require_once 'includes/header.php';
   <div class="form-wrapper">
     <h1>Register</h1>
     <p>Already have an account? <a href="login.php">Login!</a></p>
-    <form action="" method="post">
+    <form action="includes/register-inc.php" method="post">
       <input type="text" name="username" placeholder="Username">
       <input type="password" name="password" placeholder="Password">
       <input type="password" name="confirmPassword" placeholder="Confirm password">
-      <button class="btn" type="submit">Register</button>
+      <button class="btn" type="submit" name="submit">Register</button>
     </form>
   </div>
 </div>
