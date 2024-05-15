a808c2af848c1edf57ec684e1fc6c8ceb8c5de02
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-05-15 22:38:15 +0200

    add new tasks

--- basic/tasks/tasks10.php
+++ basic/tasks/tasks10.php
@@ -37,4 +37,40 @@ for ($row = 0; $row < 7; $row++) {
 }
 echo "<hr>";
 
+// task 3
+echo "<b>Task 3:</b><br><br>";
+for ($row = 0; $row < 7; $row++) {
+  for ($column = 0; $column <= 7; $column++) {
+    if (
+      ($column == 1 and ($row != 0 and $row != 6)) or
+      (($row == 0 or $row == 6) and ($column > 1 and $column < 5)) or (
+        $column == 5 and ($row == 1 or $row == 5))
+    ) {
+      echo "*";
+    } else {
+      echo " ";
+    }
+  }
+  echo "<br>";
+}
+echo "<hr>";
+
+// task 4
+echo "<b>Task 4:</b><br><br>";
+for ($row = 0; $row < 7; $row++) {
+  for ($column = 0; $column <= 7; $column++) {
+    if (
+      $column == 1 or
+      (($row == 0 or $row == 6) and ($column > 1 and $column < 5)) or
+      ($column == 5 and $row != 0 and $row != 6)
+    ) {
+      echo "*";
+    } else {
+      echo " ";
+    }
+  }
+  echo "<br>";
+}
+echo "<hr>";
+
 echo "</pre>";
