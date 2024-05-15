60be03a47bda01586d0cf348fe3409297a314b10
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-05-15 22:29:31 +0200

    add new tasks'

--- basic/tasks/task9.php
+++ basic/tasks/task9.php
@@ -163,5 +163,4 @@ for ($i = 1; $i < 10; $i++) {
 
   echo "</tr>";
 }
-
 echo '</table><br>';
---
+++ basic/tasks/tasks10.php
@@ -0,0 +1,40 @@
+<?php
+
+// task 1
+echo "<pre>";
+echo "<b>Task 1:</b><br><br>";
+for ($row = 0; $row <= 7; $row++) {
+  for ($column = 0; $column <= 7; $column++) {
+    if (
+      (($column == 1 or $column == 5) and $row != 0) or
+      (($row == 0 or $row == 3) and
+        ($column > 1 and $column < 5))
+    ) {
+      echo "*";
+    } else {
+      echo " ";
+    }
+  }
+  echo "<br>";
+}
+echo "<hr>";
+
+// task 2
+echo "<b>Task 2:</b><br><br>";
+for ($row = 0; $row < 7; $row++) {
+  for ($column = 0; $column <= 7; $column++) {
+    if (
+      $column == 1 or
+      (($row == 0 or $row == 3 or $row == 6) and ($column < 5 and $column > 1))
+      or ($column == 5 and ($row != 0 and $row != 3 and $row != 6))
+    ) {
+      echo "*";
+    } else {
+      echo " ";
+    }
+  }
+  echo "<br>";
+}
+echo "<hr>";
+
+echo "</pre>";
