ae273392348999444abac20900add5dac961f232
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-05-16 21:54:34 +0200

    add new tasks

--- basic/tasks/tasks10.php
+++ basic/tasks/tasks10.php
@@ -95,9 +95,11 @@ echo "<hr>";
 echo "<b>Task 6:</b><br><br>";
 for ($row = 0; $row < 7; $row++) {
   for ($column = 0; $column <= 7; $column++) {
-    if ($column == 1 or 
-    ($row == 0 and $column > 1 and $column < 6) or 
-    ($row == 3 and $column > 1 and $column < 5)) {
+    if (
+      $column == 1 or
+      ($row == 0 and $column > 1 and $column < 6) or
+      ($row == 3 and $column > 1 and $column < 5)
+    ) {
       echo "*";
     } else {
       echo " ";
@@ -108,4 +110,54 @@ for ($row = 0; $row < 7; $row++) {
 echo "<hr>";
 
 // task 7
+echo "<b>Task 7:</b><br><br>";
+echo '<table border ="1" style="border-collapse: collapse">';
+
+for ($row = 1; $row <= 10; $row++) {
+  echo "<tr>";
+
+  for ($column = 1; $column <= 10; $column++) {
+    $p = $column * $row;
+    echo "<td width=\"30px\">$p</td>";
+  }
+
+  echo "</tr>";
+}
+
+echo "</table>";
+echo "<br><hr>";
+
+// task 8
+function findFactorial($num)
+{
+  if ($num == 0) {
+    return 1;
+  } else {
+    return $num * findFactorial($num - 1);
+  }
+}
+
+echo "<b>Task 8:</b><br><br>";
+echo findFactorial(4) . "<br>";
+echo findFactorial(6) . "<br>";
+echo "<hr>";
+
+// task 9
+function isPrime($num)
+{
+  $result = 1;
+
+  for ($i = 2; $i < $num; $i++) {
+    if ($num % $i == 0) {
+      $result = 0;
+    }
+  }
+
+  return $num . ($result ? " is a Prime Number." : " isn't a Prime Number.") . "<br>";
+}
+
+echo "<b>Task 9:</b><br><br>";
+echo isPrime(3);
+echo isPrime(6);
+echo "<hr>";
 echo "</pre>";
