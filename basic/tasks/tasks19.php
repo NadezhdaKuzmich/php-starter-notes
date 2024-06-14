46b2a0cf6b167c1d6009a1a6c44703459316f557
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-06-15 00:27:24 +0200

    add new tasks

--- basic/tasks/tasks19.php
+++ basic/tasks/tasks19.php
@@ -13,3 +13,29 @@ if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
   echo 'This is a server not using Windows!' . "<br>";
 }
 echo "<hr>";
+
+// task 2
+echo "<b>Task 2:</b><br>";
+$a = 15;
+$b = 276;
+
+echo "Before swapping:  " . $a . ', ' . $b . "<br>";
+// list - використовується для того, щоб присвоїти списку змінних 
+// значення за одну операцію:
+list($a, $b) = [$b, $a];
+
+echo "After swapping:  " . $a . ', ' . $b . "<hr>";
+
+// task 3
+function removeDuplicates($list1)
+{
+  // array_values - повертає індексний масив з усіма значеннями масиву.
+  // array_unique - повертає новий масив без повторюваних значень:
+  $nums_unique = array_values(array_unique($list1));
+  return $nums_unique;
+}
+
+echo "<b>Task 3:</b><br><pre>";
+$nums = [1, 1, 2, 2, 3, 4, 5, 5];
+print_r(removeDuplicates($nums));
+echo "</pre><hr>";
