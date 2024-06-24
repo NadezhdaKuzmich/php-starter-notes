9629e456dfe147aa8d771d336cff5a2ba81a9e7e
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-06-24 22:09:43 +0200

    add new tasks

--- basic/tasks/tasks21.php
+++ basic/tasks/tasks21.php
@@ -21,3 +21,50 @@ print_r(take([1, 2, 3], 1));
 echo "<br>";
 print_r(take([1, 2, 3, 4, 5], 2));
 echo "</pre>" . "<hr>";
+
+// task 3
+function without($items, ...$params)
+{
+  return array_values(array_diff($items, $params));
+}
+
+echo "<b>Task 3:</b><br>" . "<pre>";
+print_r(without([2, 1, 2, 3], 1, 2));
+echo "</pre>" . "<hr>";
+
+// task 4
+function orderBy($items, $attr, $order)
+{
+  $sortedItems = [];
+
+  foreach ($items as $item) {
+    $key = $item[$attr];
+    $sortedItems[$key] = $item;
+  }
+
+  if ($order === 'desc') {
+    // krsort - сортує масив за ключами у порядку зменшення, 
+    // зберігаючи відношення між ключами і значеннями:
+    krsort($sortedItems);
+  } else {
+    // ksort - сортує масив за ключами у порядку зростання, 
+    // зберігаючи відношення між ключами і значеннями:
+    ksort($sortedItems);
+  }
+
+  return array_values($sortedItems);
+}
+
+echo "<b>Task 4:</b><br>" . "<pre>";
+print_r(
+  orderBy(
+    [
+      ['id' => 2, 'name' => 'Red'],
+      ['id' => 3, 'name' => 'Black'],
+      ['id' => 1, 'name' => 'Green']
+    ],
+    'id',
+    'desc'
+  )
+);
+echo "</pre>" . "<hr>";
