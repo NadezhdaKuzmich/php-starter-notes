eb9c5e43eec3be47fb5f5ec71047bbe526867300
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-06-24 21:55:44 +0200

    add new task

--- basic/tasks/tasks21.php
+++ basic/tasks/tasks21.php
@@ -9,3 +9,15 @@ function tail($items)
 echo "<b>Task 1:</b><br>" . "<pre>";
 print_r(tail([1, 2, 3]));
 echo "</pre>" . "<hr>";
+
+// task 2
+function take($items, $count = 1)
+{
+  return array_slice($items, 0, $count);
+}
+
+echo "<b>Task 2:</b><br>" . "<pre>";
+print_r(take([1, 2, 3], 1));
+echo "<br>";
+print_r(take([1, 2, 3, 4, 5], 2));
+echo "</pre>" . "<hr>";
