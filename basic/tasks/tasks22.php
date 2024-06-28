fef77f8df2e13b712cef9457c42386a71d187cd1
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-06-28 21:12:23 +0200

    add new task

--- basic/tasks/tasks22.php
+++ basic/tasks/tasks22.php
@@ -10,3 +10,15 @@ echo "<b>Task 1:</b><br>";
 echo editStr("C Sharp") . "<br>";
 echo editStr("JS") . "<br>";
 echo editStr("a") . "<hr>";
+
+// task 2
+function changeStr($str)
+{
+  $lastChar = substr($str, strlen($str) - 1);
+  return $lastChar . $str . $lastChar;
+}
+
+echo "<b>Task 2:</b><br>";
+echo changeStr("Red") . "<br>";
+echo changeStr("Green") . "<br>";
+echo changeStr("1") . "<hr>";
