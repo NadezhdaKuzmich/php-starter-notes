42366553f13f7dd6de6231e7ee29d92d8ad2b14b
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-07-04 23:15:03 +0200

    add new tasks

--- basic/tasks/tasks23.php
+++ basic/tasks/tasks23.php
@@ -1,12 +1,28 @@
 <?php
 
 // task 1
-function test($str)
+function returnRepeatedLastChars($str)
 {
   $lastChars = substr($str, strlen($str) - 2);
   return str_repeat($lastChars, 3);
 }
 
 echo "<b>Task 1:</b><br>";
-echo test("Hello") . "<br>";
-echo test("Hi") . "<hr>";
+echo returnRepeatedLastChars("Hello") . "<br>";
+echo returnRepeatedLastChars("Hi") . "<hr>";
+
+// task 2
+function calculateSum($nums)
+{
+  $result = 0;
+
+  foreach ($nums as $num) {
+    $result += $num;
+  }
+  
+  return $result;
+}
+
+echo "<b>Task 2:</b><br>";
+echo calculateSum([10, 20, 30, 40, 50]) . "<br>";
+echo calculateSum([10, 20, -30, -40, 50]) . "<hr>";
