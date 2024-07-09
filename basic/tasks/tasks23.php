fb00f1f1022bb0c6efa04ecde3a0739de932dbec
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-07-09 21:35:35 +0200

    add new tasks

--- basic/tasks/tasks23.php
+++ basic/tasks/tasks23.php
@@ -137,3 +137,32 @@ echo "<b>Task 9:</b><br>";
 $result = combineArrays([10, 20, 30], [40, 50, 60]);
 echo "New array: " . implode(', ', $result);
 echo "<hr>";
+
+// task 10
+function findLargestSum($arr1, $arr2)
+{
+  $sum1 = 0;
+  $sum2 = 0;
+
+  if (count($arr1) === 3 && count($arr2) === 3) {
+    for ($i = 0; $i < 3; $i++) {
+      $sum1 += $arr1[$i];
+      $sum2 += $arr2[$i];
+    }
+  } else {
+    return 'Inappropriate array length.';
+  }
+
+  return ($sum1 > $sum2
+    ? $arr1
+    : $sum1 === $sum2)
+    ? 'The sums of the arrays are the same.'
+    : $arr2;
+}
+
+echo "<b>Task 10:</b><br>";
+$result = findLargestSum([10, 20, -30], [10, 20, 30]);
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+echo "<hr>";
