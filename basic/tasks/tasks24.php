434f75dd50726a7bba5b95754d361230f76fe291
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-07-10 21:58:02 +0200

    add new tasks

--- basic/tasks/tasks24.php
+++ basic/tasks/tasks24.php
@@ -10,3 +10,19 @@ echo "<b>Task 1:</b><br>";
 $arr = [10, 20, -30, -40, 50];
 $result = reverseArr($arr);
 echo "Reverse array: " . implode(', ', $result) . "<hr>";
+
+// task 2
+function calculateSum($arr)
+{
+  $result = 0;
+
+  foreach ($arr as $item) {
+    $result += $item;
+  }
+  
+  return $result;
+}
+
+echo "<b>Task 2:</b><br>";
+echo calculateSum([10, 20, 30, 40, 50]) . "<br>";
+echo calculateSum([10, 20, -30, -40, 50]) . "<hr>";
