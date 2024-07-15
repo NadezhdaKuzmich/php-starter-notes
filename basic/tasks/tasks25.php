c3f393f2a1ffe277bad99e99debe542ed8292f54
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-07-15 20:39:13 +0200

    add new tasks

--- basic/tasks/tasks25.php
+++ basic/tasks/tasks25.php
@@ -11,3 +11,17 @@ $_SESSION["preferences"] = $userPreferences;
 
 echo "<b>Task 1:</b><br>";
 echo "User preferences have been stored in the session variable 'preferences'.<hr>";
+
+// task 2
+echo "<b>Task 2:</b><br>";
+if (isset($_SESSION["preferences"])) {
+  $userPreferences = $_SESSION["preferences"];
+
+  echo "User Preferences:</br>";
+  foreach ($userPreferences as $key => $value) {
+    echo "$key: $value </br>";
+  }
+} else {
+  echo "No user preferences found.";
+}
+echo "<hr>";
