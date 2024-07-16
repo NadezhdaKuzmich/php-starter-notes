4fbce77306d0ae0c3e64c963ea6099875fef672b
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-07-16 21:58:19 +0200

    add new tasks

--- basic/tasks/tasks25.php
+++ basic/tasks/tasks25.php
@@ -54,3 +54,15 @@ session_regenerate_id(true);
 
 echo "<b>Task 4:</b><br>";
 echo "Session ID has been regenerated.<hr>";
+
+// task 5
+echo "<b>Task 5:</b><br>";
+if (isset($_SESSION['last_access_time'])) {
+  $lastAccessTime = $_SESSION['last_access_time'];
+  echo "Last access time: " . date('Y-m-d H:i:s', $lastAccessTime);
+  $_SESSION['last_access_time'] = time();
+} else {
+  $_SESSION['last_access_time'] = time();
+  echo "Session started. First access.";
+}
+echo "<hr>";
