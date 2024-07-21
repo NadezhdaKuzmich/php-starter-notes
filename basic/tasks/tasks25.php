1fe5ed1a0e5b3410f2f500d08f84bd36eb95b941
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-07-21 22:07:47 +0200

    add new tasks

--- basic/tasks/tasks25.php
+++ basic/tasks/tasks25.php
@@ -218,3 +218,45 @@ $student->age = 16;
 $student->grade = 10;
 $student->displayInfo();
 echo "<hr>";
+
+// task 12
+class Validation
+{
+  public static function validateEmail($email)
+  {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+  }
+
+  public static function validatePassword($password)
+  {
+    return strlen($password) >= 8;
+  }
+
+  public static function validateField($field)
+  {
+    return !empty($field);
+  }
+}
+
+$email = "admin@example.com";
+$password = "password123";
+$field = "";
+
+echo "<b>Task 12:</b><br>";
+if (Validation::validateEmail($email)) {
+  echo "Email is valid.<br>";
+} else {
+  echo "Email is invalid.<br>";
+}
+
+if (Validation::validatePassword($password)) {
+  echo "Password is valid.<br>";
+} else {
+  echo "Password is invalid.<br>";
+}
+
+if (Validation::validateField($field)) {
+  echo "Field is valid.<br>";
+} else {
+  echo "Field is invalid.<br>";
+}
