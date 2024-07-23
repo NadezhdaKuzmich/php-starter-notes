36025a5c29717e352dcb19a2b234799a53dcc1b2
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-07-23 19:25:55 +0200

    add new tasks

--- basic/tasks/tasks26.php
+++ basic/tasks/tasks26.php
@@ -111,3 +111,40 @@ $calculator->subtract(3);
 $result = $calculator->getResult();
 echo "Result: $result";
 echo "<hr>";
+
+// task 4
+class File
+{
+  public $name;
+  public $size;
+
+  public function __construct($name, $size)
+  {
+    $this->name = $name;
+    $this->size = $size;
+  }
+
+  public static function calculateTotalSize($files)
+  {
+    $totalSize = 0;
+
+    foreach ($files as $file) {
+      if ($file instanceof File) {
+        $totalSize += $file->size;
+      }
+    }
+
+    return $totalSize;
+  }
+}
+
+echo "<b>Task 4:</b><br>";
+$file1 = new File("file1.txt", 1000);
+$file2 = new File("file2.txt", 2000);
+$file3 = new File("file3.txt", 1500);
+
+$files = [$file1, $file2, $file3];
+$totalSize = File::calculateTotalSize($files);
+
+echo "Total size of files: $totalSize bytes";
+echo "<hr>";
