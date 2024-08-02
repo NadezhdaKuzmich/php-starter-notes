df5f2332f1807e3e96a1edd1b6b939df39c78b8a
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-08-02 22:38:08 +0200

    add new theme

---
+++ basic/tasks/components/header.php
@@ -0,0 +1,10 @@
+<?php
+echo '<nav>
+  <ul>
+    <li><a href="#">Learn Online Free Courses</a></li>
+    <li><a href="#">Web Tutorials</a></li>
+    <li><a href="#">Articles</a></li>
+    <li><a href="#">Interview Questions</a></li>
+    <li><a href="#">Tips for IT Professionals</a></li>
+  </ul>
+</nav>';
---
+++ basic/tasks/tasks28x1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <?php include 'components/header.php'; ?>
+  <h1>Welcome to our website!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus temporibus praesentium esse voluptas, vitae quidem!
+    Labore dolore mollitia repellendus vel pariatur esse quia officiis, quibusdam itaque neque reprehenderit placeat
+    officia voluptatem tenetur iure sunt totam. Numquam molestias explicabo voluptatum blanditiis est magni
+    exercitationem esse, fuga at ab dolorem quibusdam vel!</p>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio magnam eos, facilis in incidunt distinctio aut
+    repudiandae nulla, explicabo pariatur ut saepe ea illo asperiores exercitationem quibusdam vel ducimus beatae,
+    voluptatem assumenda? Fuga ipsam expedita provident blanditiis nihil, eius, enim earum qui eveniet modi aliquid
+    rerum. Modi dignissimos delectus temporibus excepturi est repudiandae earum architecto, doloribus dolorem porro
+    eligendi a saepe ad tenetur enim, maxime sint cumque explicabo! Molestiae illo tempora quia tempore modi repellendus
+    magni sequi dolorem, cupiditate temporibus, dolores sapiente quaerat explicabo maiores aspernatur corrupti libero
+    quod facilis ea. Libero, explicabo. Reprehenderit harum, qui mollitia odio autem at ullam aliquid architecto. Earum
+    iure, repellendus ratione saepe facilis optio nostrum voluptate veritatis cupiditate vitae incidunt quis!
+    Perferendis numquam id vel fugiat libero reprehenderit corporis. Blanditiis quos consectetur explicabo illum enim
+    molestias autem quidem minus iure ullam sed reprehenderit sapiente error nam, voluptatum molestiae reiciendis illo
+    quaerat quasi, magni fugiat aut modi odit harum. Voluptatum voluptatem rerum itaque inventore nesciunt eligendi
+    suscipit ipsam consectetur laborum velit. Hic ea repellat molestias voluptas ex, necessitatibus, libero consequatur
+    exercitationem minus inventore nesciunt nihil soluta excepturi suscipit beatae repellendus alias temporibus
+    voluptate aliquam fuga? Vel ut blanditiis corporis perspiciatis ducimus voluptatem voluptates consequuntur expedita.
+  </p>
+
+</body>
+
+</html>
