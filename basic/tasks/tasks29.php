dfba2d51e4013699f10b2f5589ea431b27fd6391
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-08-10 23:27:22 +0200

    add new tasks

--- basic/tasks/tasks29.php
+++ basic/tasks/tasks29.php
@@ -11,3 +11,15 @@ for ($i = 0; $i < strlen($num); $i++) {
 echo "<b>Task 1:</b><br>";
 echo "Sum of digits 14597 is $sum";
 echo "<hr>";
+
+// task 2
+$seasons = ["Winter", "Spring", "Summer", "Autumn"];
+
+echo "<b>Task 2:</b><br>";
+foreach ($seasons as $season) {
+  echo $season;
+  echo $season !== $seasons[count($seasons) - 1]
+    ? ', '
+    : '.';
+}
+echo "<hr>";
