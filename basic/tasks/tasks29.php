3b77990274c746a109e064560cab529427de34e3
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-08-11 21:36:04 +0200

    add new tasks

--- basic/tasks/tasks29.php
+++ basic/tasks/tasks29.php
@@ -30,9 +30,11 @@ for ($i = 1; $i <= 5; $i++) {
   for ($k = 5; $k >= $i; $k--) {
     echo " ";
   }
+
   for ($j = 1; $j <= $i; $j++) {
     echo "* ";
   }
+
   echo "<br>";
 }
 
@@ -40,9 +42,34 @@ for ($i = 4; $i >= 1; $i--) {
   for ($k = 5; $k >= $i; $k--) {
     echo " ";
   }
+
   for ($j = 1; $j <= $i; $j++) {
     echo "* ";
   }
+
   echo "<br>";
 }
 echo "</pre><hr>";
+
+// task 4
+echo "<b>Task 4:</b><br>";
+for ($i = 5; $i >= 1; $i--) {
+  if ($i % 2 != 0) {
+    for ($j = 5; $j >= $i; $j--) {
+      echo "* ";
+    }
+
+    echo "<br>";
+  }
+}
+
+for ($i = 2; $i <= 5; $i++) {
+  if ($i % 2 != 0) {
+    for ($j = 5; $j >= $i; $j--) {
+      echo "* ";
+    }
+
+    echo "<br>";
+  }
+}
+echo "<hr>";
