af3d9305015502d6111f8785cc550866d5b85869
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-08-15 22:10:23 +0200

    add new task

--- basic/tasks/tasks29.php
+++ basic/tasks/tasks29.php
@@ -96,3 +96,45 @@ switch ($ch) {
     break;
 }
 echo "<hr>";
+
+// task 6
+$car = "Hyundai";
+$model = "Tucson";
+
+echo "<b>Task 6:</b><br>";
+switch ($car) {
+  case "Honda":
+    switch ($model) {
+      case "Amaze":
+        echo "Honda Amaze price is 5.93 - 9.79 Lakh.";
+        break;
+      case "City":
+        echo "Honda City price is 9.91 - 14.31 Lakh.";
+        break;
+    }
+    break;
+  case "Renault":
+    switch ($model) {
+      case "Duster":
+        echo "Renault Duster price is 9.15 - 14.83 L.";
+        break;
+      case "Kwid":
+        echo "Renault Kwid price is 3.15 - 5.44 L.";
+        break;
+    }
+    break;
+  case "Hyundai":
+    switch ($model) {
+      case "Creta":
+        echo "Hyundai Creta price is 11.42 - 18.73 L.";
+        break;
+      case "Tucson":
+        echo "Hyundai Tucson price is 22.39 - 32.07 L.";
+        break;
+      case "Xcent":
+        echo "Hyundai Xcent price is 6.5 - 10.05 L.";
+        break;
+    }
+    break;
+}
+echo "<hr>";
