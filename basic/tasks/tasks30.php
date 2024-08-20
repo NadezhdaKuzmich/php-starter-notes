27698f5f75f915b3d3098f6c128cba9ee1f5394e
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-08-20 23:26:24 +0200

    add new task

--- basic/tasks/tasks30.php
+++ basic/tasks/tasks30.php
@@ -12,8 +12,17 @@ echo "<hr>";
 $str = "PHP is Lovely Language!";
 $strArray = count_chars($str, 1);
 
-echo "<b>Task 1:</b><br>";
+echo "<b>Task 2:</b><br>";
 foreach ($strArray as $key => $value) {
   echo "The character <b>'" . chr($key) . "'</b> was found $value time(s)<br>";
 }
 echo "<hr>";
+
+// task 4
+$str = "Hello, we are here to help you.";
+
+echo "<b>Task 3:</b><pre>";
+print_r(explode(" ", $str, 0));
+print_r(explode(" ", $str, 4));
+print_r(explode(" ", $str, -3));
+echo "</pre><hr>";
