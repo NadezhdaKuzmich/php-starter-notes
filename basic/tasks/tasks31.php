04c9beb46b6537c88506229e48497fa3eec8aed0
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-08-28 22:54:44 +0200

    add new task

--- basic/tasks/tasks31.php
+++ basic/tasks/tasks31.php
@@ -7,3 +7,8 @@ echo "$replace1 <br>";
 
 $replace2 = substr_replace("Hello JavaScript", "PHP", -10);
 echo "$replace2 <hr>";
+
+// task 2
+echo "<b>Task 2:</b><br>";
+$replace = substr_replace("PHP!", "Hello ", 0, 0);
+echo "$replace1 <hr>";
