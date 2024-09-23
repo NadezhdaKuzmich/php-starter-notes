b8cdc733eba834fca58d911c37714eb462790e85
NadezhdaKuzmich <nadiia.kuzmich@md.p2h.com>
2024-09-23 21:40:48 +0200

    add new tasks

--- advanced/database/index.php
+++ advanced/database/index.php
@@ -1,14 +1,14 @@
 <?php
 
 $serverName = "localhost";
-$username = "root@localhost";
-$password = "";
+$username = "root";
+$password = "root";
 
 try {
-  $connection = new PDO("msql:host=$serverName;dbname=epic_database", $username, $password);
+  $connection = new PDO("mysql:host=$serverName;dbname=epic", $username, $password);
   $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   echo 'Connected successfully!';
 } catch (PDOException $error) {
-  echo "Connection failed: " . $error;
+  echo "Connection failed: $error";
 }
