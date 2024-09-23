ee0fdb3d4e011e4141aef97ae8b0dc4e305cf3b5
NadezhdaKuzmich <nadiia.kuzmich@md.p2h.com>
2024-09-23 21:59:37 +0200

    add new task

--- advanced/database/index.php
+++ advanced/database/index.php
@@ -8,7 +8,18 @@ try {
   $connection = new PDO("mysql:host=$serverName;dbname=epic", $username, $password);
   $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  echo 'Connected successfully!';
+  echo 'Connected successfully!<br>';
 } catch (PDOException $error) {
   echo "Connection failed: $error";
 }
+
+$query = $connection->prepare("SELECT id, name FROM Users");
+$query->execute();
+
+$result = $query->setFetchMode(PDO::FETCH_ASSOC);
+
+foreach (new RecursiveArrayIterator($query->fetchAll()) as $key => $value) {
+  echo $value["name"] . "<br>";
+}
+
+$connection = null;
