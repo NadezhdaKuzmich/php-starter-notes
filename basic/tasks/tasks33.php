58aaa1ea49bf2a03aca6ef86b4b41d6ed603afcc
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-09-27 23:56:45 +0200

    add new task

--- basic/tasks/tasks33.php
+++ basic/tasks/tasks33.php
@@ -4,3 +4,30 @@
 echo "<b>Task 1:</b><br>";
 echo 'Current PHP version: ' . phpversion();
 echo "<hr>";
+
+// task 2
+class Fruit
+{
+  public $name;
+  public $color;
+
+  function setName($name)
+  {
+    $this->name = $name;
+  }
+  function getName()
+  {
+    return $this->name;
+  }
+}
+
+$apple = new Fruit();
+$banana = new Fruit();
+$apple->setName('Apple');
+$banana->setName('Banana');
+
+echo "<b>Task 2:</b><br>";
+echo $apple->getName();
+echo "<br>";
+echo $banana->getName();
+echo "<hr>";
