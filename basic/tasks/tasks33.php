be1f2542f846daeda665779a3193c9df91488aa2
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-09-30 23:02:51 +0200

    add new theme

--- basic/tasks/tasks33.php
+++ basic/tasks/tasks33.php
@@ -31,3 +31,22 @@ echo $apple->getName();
 echo "<br>";
 echo $banana->getName();
 echo "<hr>";
+
+// task 3
+$favcolor = "red";
+
+echo "<b>Task 3:</b><br>";
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+echo "<hr>";
