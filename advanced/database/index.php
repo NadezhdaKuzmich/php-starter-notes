ccde1c2fdc2a3bd5ca322f455d0f98e1457d7f9d
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-10-17 22:11:17 +0200

    add new theme

--- advanced/database/index.php
+++ advanced/database/index.php
@@ -4,6 +4,7 @@ $serverName = "localhost";
 $username = "root";
 $password = "root";
 
+// CONNECTION
 try {
   $connection = new PDO("mysql:host=$serverName;dbname=epic", $username, $password);
   $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -15,6 +16,7 @@ try {
 
 $type = 'superuser';
 
+// SELECT (WHERE, WHERE-LIKE)
 // $query = $connection->prepare("SELECT id, name, type FROM Users");
 // $query = $connection->prepare("SELECT * FROM Users");
 // $query = $connection->prepare("SELECT * FROM Users WHERE type='superuser'");
@@ -22,10 +24,12 @@ $type = 'superuser';
 // $query = $connection->prepare("SELECT * FROM Users WHERE name LIKE '%n'"); // has to end on the 'n'
 // $query = $connection->prepare("SELECT * FROM Users WHERE name LIKE '%n%'"); // contain 'n'
 
+// INSERT
 // $query = $connection->prepare("INSERT INTO Users (name, type) VALUES ('Vadym', 'superuser'), ('Superman', 'user')");
 // $lastID = $connection->lastInsertId();
 // echo "Last id: $lastID <hr />";
 
+// MULTIPLE INSERT
 // $connection->beginTransaction();
 // $connection->exec("INSERT INTO Users (name, type) VALUES ('Venom', 'superuser')");
 // $connection->exec("INSERT INTO Users (name, type) VALUES ('Igor', 'user')");
@@ -34,15 +38,19 @@ $type = 'superuser';
 $query = $connection->prepare("UPDATE Users SET name='Superuser' WHERE id=3");
 $query->execute();
 
+// LEFT JOIN
 // $query = $connection->prepare("SELECT Users.id, Users.name, Cars.car_name FROM Users LEFT JOIN Cars ON Users.id=Cars.user_id");
 // $query->execute();
 
+// DELETE WHERE
 // $query = $connection->prepare("DELETE FROM Users WHERE id=8");
 // $query->execute();
 
+// TRUNCATE TABLE
 // $query = $connection->prepare("TRUNCATE TABLE Cars");
 // $query->execute();
 
+// DROP TABLE
 // $query = $connection->prepare(query: "DROP TABLE Cars");
 // $query->execute();
 
@@ -58,6 +66,7 @@ foreach (new RecursiveArrayIterator($query->fetchAll()) as $key => $value) {
   // echo $value["id"] . ' : ' . $value["name"] . ' - ' . ($value["car_name"] ? $value["car_name"] : 'has no car') . '<br>';
 }
 
+// CREATE TABLE
 // $query = $connection->prepare("
 // CREATE TABLE Test
 // (
@@ -66,10 +75,12 @@ foreach (new RecursiveArrayIterator($query->fetchAll()) as $key => $value) {
 // age int
 // )");
 
+// CREATE TABLE (LIKE)
 // $query = $connection->prepare("CREATE TABLE NewUsers LIKE Users");
 // $query = $connection->prepare("INSERT INTO NewUsers SELECT * FROM Users");
 // $query->execute();
 
+// ALTER COLUMN
 // $query = $connection->prepare("
 // ALTER TABLE NewUsers 
 // ADD country varchar(64)
@@ -87,3 +98,36 @@ MODIFY COLUMN name varchar(64)
 $query->execute();
 
 $connection = null;
+
+/*
+try {
+  // CREATE DATABASE
+  // $connection = new PDO("mysql:host=$serverName;", $username, $password);
+  // $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // $query = $connection->prepare("CREATE DATABASE testDB");
+  // $query->execute();
+
+  $connection = new PDO("mysql:host=$serverName;dbname=testDB", $username, $password);
+  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  echo 'Connected successfully!<br>';
+} catch (PDOException $error) {
+  echo "Connection failed: $error";
+}
+*/
+
+// $query = $connection->prepare("
+// CREATE TABLE Test
+// (
+// id int,
+// name varchar(32),
+// age int
+// )");
+// $query->execute();
+
+// DROP DATABASE
+// $query = $connection->prepare("DROP DATABASE testDB");
+// $query->execute();
+
+// $connection = null;
