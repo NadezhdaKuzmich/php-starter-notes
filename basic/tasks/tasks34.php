30e4ff5fd3f1acfaee9f1dfb62247d95a72d20c2
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-10-21 23:00:34 +0200

    add new theme

--- basic/tasks/tasks34.php
+++ basic/tasks/tasks34.php
@@ -8,3 +8,10 @@ $users = array_combine($names, $ages);
 echo "<b>Task 1:</b><br><pre>";
 print_r($users);
 echo "</pre><hr>";
+
+// task 2
+$colors = ["red", "green", "yellow"];
+
+echo "<b>Task 2:</b><br><pre>";
+print_r(array_pad($colors, 5, "blue"));
+echo "</pre><hr>";
