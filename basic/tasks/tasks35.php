054f2ca1a4868836fad5585efe4f44f3a4e0caf1
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-11-19 21:33:57 +0100

    add new task

--- basic/tasks/tasks35.php
+++ basic/tasks/tasks35.php
@@ -6,3 +6,12 @@ $nums = [5, 6];
 echo "<b>Task 1:</b><br>";
 echo array_product($nums);
 echo "<hr>";
+
+// task 2
+$users = ["Peter", "Joe", "John", "Mike"];
+
+echo "<b>Task 2:</b><br>";
+echo current($users) . "<br>";
+echo next($users) . "<br>";
+echo prev($users);
+echo "<hr>";
