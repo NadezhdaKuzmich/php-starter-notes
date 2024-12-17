756d2b1cf3efc20c2675a90609bb60341ec188ee
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-12-17 23:48:14 +0100

    add new task

--- basic/tasks/tasks36.php
+++ basic/tasks/tasks36.php
@@ -3,8 +3,10 @@
 // task 1
 $cars = ["Volvo" => "XC90", "BMW" => "X5"];
 
+echo "<b>Task 1:</b><br>";
 if (array_key_exists("Volvo", $cars)) {
   echo "Key exists!";
 } else {
   echo "Key does not exist!";
 }
+echo "<hr>";
