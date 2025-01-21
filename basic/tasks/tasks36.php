9fc24c31f8b6b5f5355db45770c904ef3f4d0d67
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2025-01-21 21:05:59 +0100

    add new task

--- basic/tasks/tasks36.php
+++ basic/tasks/tasks36.php
@@ -82,3 +82,12 @@ try {
   echo "Unable to divide.";
 }
 echo "<hr>";
+
+// task 11
+$int = 100;
+
+if (filter_var($int, FILTER_VALIDATE_INT)) {
+  echo "Integer is valid";
+} else {
+  echo "Integer is not valid";
+}
