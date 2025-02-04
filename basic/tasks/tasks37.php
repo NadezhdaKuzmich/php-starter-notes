b389f12a6c9fb474cbebdc23edac541044a64521
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2025-02-04 22:44:10 +0100

    add new task

--- basic/tasks/tasks37.php
+++ basic/tasks/tasks37.php
@@ -10,3 +10,7 @@ echo "</pre><hr>";
 // task 2
 $numbers = [1, 2, 3, 4];
 $even = array_filter($numbers, fn($val) => $val % 2 === 0);
+
+echo "<b>Task 2:</b><br><pre>";
+var_dump($even);
+echo "</pre><hr>";
