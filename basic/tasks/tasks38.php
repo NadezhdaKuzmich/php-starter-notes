9861d50933cb7d8b77cb1c472847223f2310eeac
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2025-03-15 21:45:48 +0100

    add new task

--- basic/tasks/tasks38.php
+++ basic/tasks/tasks38.php
@@ -7,3 +7,10 @@ echo "<b>Task 1:</b><br><pre>";
 var_dump(array_slice($letters, 2, -1));
 var_dump(array_slice($letters, 2, -1, true));
 echo "</pre><hr>";
+
+// task 2
+$arr = ["1" => "a", "2" => "b", "3" => "c"];
+
+echo "<b>Task 2:</b><br>";
+print_r(array_rand($arr));
+echo "<hr>";
